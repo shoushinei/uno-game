0379520d2e05050d52a5068cf7a8910b774beb83
shoushinei <shoushinei9@gmail.com>
2026-06-24 22:39:52 +0900

    Firebase認証機能を追加し、Googleプロバイダーを設定

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -19,3 +19,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
+
+export const auth = getAuth(app); 
+export const googleProvider = new GoogleAuthProvider();
